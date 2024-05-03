a799fa606683c29494fc47f560c1b03b3ec14e66
Sumanth K R. <sumanth.kr@excelindia.com>
2024-05-03 01:40:51 -0700

    updated

---
+++ react1/apifetch/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+// import Imag2 from './component/Imag2';
+// import Asnpage2 from './component/Asnpage2';
+// import  Rtask from './component/Rtask'
+// import VariationsExample3 from './component/VariationExample3'
+// import Shape from './component/Shape';
+// import Reducer from './component/Reducer';
+
+// import Asanpage1 from './component/Asanpage1';
+// import Asanpage3 from './component/Asanpage3';
+
+// import Img from './component/Img';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Portfolio from './component/Portfolio';
+// import About from './component/About';
+
+import Api from './component/Api'
+
+// import { Book } from '@mui/icons-material';
+function App() {
+  return (
+    <div >
+  
+     
+    
+    <Api/>
+   
+    </div>
+  )
+}
+
+export default App;
---
+++ react1/wix_task/src/App.js
@@ -0,0 +1,12 @@
+import MainRouter from "./components/MainDriver/MainRouter";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <MainRouter />
+    </>
+  );
+}
+
+export default App;
